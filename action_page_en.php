41221065e4cde2c5de5ecc5d02b881e3e908fd38
cancelaconcepts <cancelaconcepts@gmail.com>
2016-10-15 19:50:10 +0100

    V2.1
    Some small changes

--- action_page_en.php
+++ action_page_en.php
@@ -1,6 +1,6 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "fcancela@student.dei.uc.pt"; // this is your Email address
+    $to = "cancelaconcepts@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
     $subject = "Cancela Concepts - Message";
